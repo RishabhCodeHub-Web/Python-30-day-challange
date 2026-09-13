938ed3e69d60eb08559f70a2aad55f67f878eedd
RishabhCodeHub-Web <rishabhpandeyphone@gmail.com>
2026-09-13 20:58:16 +0530

    Add files via upload

---
+++ Day2_2nd_class_Program.py
@@ -0,0 +1,22 @@
+Name = "Rishabh"
+Age = 19
+Age2 = Age
+print (Age2)
+
+
+
+Name = "Rishabh"
+Age = 19
+Percentage = 91.55
+
+print (type(Name))
+print (type(Age))
+print (type(Percentage))
+
+
+age = 19
+old = True
+a = None
+print (type(old))
+print (type(a))
+print (" Always remamber Boolean data type (True,False) will be start on capital letter")
