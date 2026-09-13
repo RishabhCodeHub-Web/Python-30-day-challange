6d02866183a8a55d363a5b646fcb0c2bc6705268
RishabhCodeHub-Web <rishabhpandeyphone@gmail.com>
2026-09-13 20:57:50 +0530

    Add files via upload

---
+++ day2_program.py
@@ -0,0 +1,19 @@
+print ("First class")
+print ("today we will see Different between - Number, string, variable and TypeError")
+
+print (" 1) Number")
+print (10+10)
+print ("The output is 20 because both are numbers thats why python added")
+
+print (" 2) String")
+print ("10"+"10")
+print ("The output is 1020 because of inverted comma they become string (text)")
+
+print ("3) Variable")
+score = 77
+print (score)
+print ("The output is 77 because score is a variable and its value is 77, so when we called 'score', it outputted its value.")
+
+print ("4) TypeError ")
+print ("99" + 33)
+print ("The output error occurred because'99' is a string (since it is enclosed in quotes), while 33 is a number therefore, they cannot be added together.")
