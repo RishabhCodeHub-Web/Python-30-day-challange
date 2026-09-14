400848d0ad2b6ca2eeaf3dd5eb5dd3421802bde1
RishabhCodeHub-Web <rishabhpandeyphone@gmail.com>
2026-09-14 23:47:45 +0530

    Add files via upload

---
+++ Day3_Program.py
@@ -0,0 +1,42 @@
+a = 9594549
+b = 4646984
+sum = a+b
+print (sum)
+
+
+# Comment in Python 
+
+print ("Its my third class of python")
+
+# print ("Its my third class of python")
+""" print ("Its my third class of python")
+print ("Its my third class of python")
+print ("Its my third class of python") """
+
+
+#Arithmetic Operators
+a = 85
+b = 92
+print (a + b)
+print (a - b)
+print (a * b)
+print (a / b)
+print (a ** b)
+print (a % b) #for find reminders
+
+
+#relational/comparison operators
+a = 80
+b = 55
+print (a == b)    #in relational case answers will be always come on boolen value (True/False)
+print (a != b)
+print (a > b)
+print (a < b)
+print (a >= b)
+print (a <= b)
+
+
+#Assignment operators
+num = 10
+num += 10            #10+10 => 20
+print ("num :-", num)
